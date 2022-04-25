ba2a87c479f24985d8194c1827df15563f11ad9f
OlivierLeufkens <103988497+OlivierLeufkens@users.noreply.github.com>
2022-04-25 10:15:42 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,11 @@
 // JavaScript Document
-console.log("Howdy!");
+console.log("Evolve!");
+
+var deButton = document.querySelector("button");
+var jolteon = document.querySelector("img");
+
+deButton.addEventListener("click", EvolveJolteon);
+
+function EvolveJolteon() {
+	jolteon.classList.toggle("ThunderstoneEvolve");
+}
